7d4782c386850d1911f012d34b4ef8d0b69847f3
Artem Vorotnikov <artem@vorotnikov.me>
2015-08-08 21:34:17 +0300

    [Update] Gen script creates only missing files and uses /tmp folder now

--- create_locales.sh
+++ create_locales.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 NEW_LINGUAS=$1
 FOLDERS=$2
+TMPDIR="/tmp/locale_temp"
 
 for lang in $NEW_LINGUAS
 do
   for folder in $FOLDERS
   do
-    mkdir $folder/po/$lang
+    mkdir -p $folder/po/$lang
+    mkdir -p $TMPDIR
     echo ""
-    cp -r $folder/pot/* $folder/po/$lang
+    cp -r $folder/pot/* $TMPDIR
     
-    FILES=$(find $folder/po/$lang -printf "%P\n" | grep .pot)
+    FILES=$(find $TMPDIR -printf "%P\n" | grep .pot)
     for file in $FILES
     do
       basepath=$(echo $file | sed "s|.pot||")
-      echo "Moving file $basepath.pot to $basepath.po"
-      mv $folder/po/$lang/$basepath.pot $folder/po/$lang/$basepath.po
+      if !([ -r $folder/po/$lang/$basepath.po ])
+      then
+        echo "Moving file $basepath.pot to $basepath.po"
+        install -D $TMPDIR/$basepath.pot $folder/po/$lang/$basepath.po
+      fi
     done
+    rm -rf $TMPDIR
   done
 done
